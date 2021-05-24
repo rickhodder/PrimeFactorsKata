694df35b95c726ff6e17a01de82a9cd21991b2b6
rickhodder <rickhodder@msn.com>
2021-05-23 21:41:33 -0400

    Two has no factors

--- PrimeFactorsTests.cs
+++ PrimeFactorsTests.cs
@@ -8,7 +8,15 @@ namespace PrimeFactorsKata
     public class PrimeFactorsTests
     {
         [Fact]
-        public void Generate_Test()
+        public void Generate_TwoHasNoFactors()
+        {
+            var factors = PrimeFactors.Generate(2);
+            Assert.Equal(0,factors.Count());
+        }
+
+
+        [Fact]
+        public void Generate_OneHasNoFactors()
         {
             var factors = PrimeFactors.Generate(1);
             Assert.Equal(0,factors.Count());
